0909c01ac57fb5c2dd760511e2ae5ec327f31b03
Wenceslau Neto <wbaneto@gmail.com>
2024-10-24 21:32:56 +0100

    feat: service video encoder

--- encoder/application/services/video_service.go
+++ encoder/application/services/video_service.go
@@ -6,7 +6,7 @@ import (
 	"encoder/application/repositories"
 	"encoder/domain"
 	log "github.com/sirupsen/logrus"
-	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 )
@@ -43,7 +43,7 @@ func (v *VideoService) Download(bucketName string) error {
 	defer r.Close()
 
 	// Read the content of the file
-	body, err := io.ReadAll(r)
+	body, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
 	}
@@ -85,6 +85,63 @@ func (v *VideoService) Fragment() error {
 	printOutput(output)
 
 	return nil
+}
+
+func (v *VideoService) Encode() error {
+	cmdArgs := []string{}
+	// Path to the mp4dash binary
+	cmdArgs = append(cmdArgs, os.Getenv("LOCAL_STORAGE_PATH")+"/"+v.Video.ID+".frag")
+
+	// Command to segment the video
+	cmdArgs = append(cmdArgs, "--use-segment-timeline")
+
+	// Command
+	cmdArgs = append(cmdArgs, "-o")
+
+	// Path to save the video
+	cmdArgs = append(cmdArgs, os.Getenv("LOCAL_STORAGE_PATH")+"/"+v.Video.ID)
+
+	// Command
+	cmdArgs = append(cmdArgs, "-f")
+
+	// Path where the command will be executed
+	cmdArgs = append(cmdArgs, "--exec-dir")
+	cmdArgs = append(cmdArgs, "/opt/bento4/bin/")
+
+	// Execute the command with all the arguments
+	cmd := exec.Command("mp4dash", cmdArgs...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	printOutput(output)
+
+	return nil
+}
+
+func (v *VideoService) Finish() error {
+	err := os.Remove(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		log.Println("Error removing the video: ", err)
+		return err
+	}
+
+	err = os.Remove(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag")
+	if err != nil {
+		log.Println("Error removing the frag: ", err)
+		return err
+	}
+
+	err = os.RemoveAll(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID)
+	if err != nil {
+		log.Println("Error removing the folder with the fragments: ", err)
+		return err
+	}
+
+	log.Println("Files have been removed", v.Video.ID)
+
+	return err
+
 }
 
 func printOutput(output []byte) {
--- encoder/application/services/video_service_test.go
+++ encoder/application/services/video_service_test.go
@@ -1,1 +1,56 @@
-package services
+package services_test
+
+import (
+	"encoder/application/repositories"
+	"encoder/application/services"
+	"encoder/domain"
+	"encoder/framework/database"
+	godotenv "github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"log"
+	"testing"
+	"time"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "resource-5fe9b506258d4b8290178ee01f06df38-file_example_mp4_1920_18mg.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("sysm_catalogo_videos/project-299e23d88f8643bd9acf1833de39144b")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	err = videoService.Finish()
+	require.Nil(t, err)
+}
